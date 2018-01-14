08b4c2712ba787aac7d2c9d5b42ea0626f20ffd6
J.Azimi <jalalazimi@gmail.com>
2018-01-14 10:41:27 +0330

    update comment of list methods

--- src/list.js
+++ src/list.js
@@ -6,6 +6,9 @@ export default class List {
     this.dataStore = [];
   }
 
+  /**
+   * Clears all elements from list
+   */
   clear() {
     delete this.dataStore;
     this.dataStore = [];
@@ -34,6 +37,12 @@ export default class List {
     return this.dataStore;
   }
 
+  /**
+   * Inserts new element after existing element
+   * @param element
+   * @param after
+   * @return {boolean}
+   */
   insert(element, after) {
     const insertPos = this.find(after);
 
@@ -45,10 +54,19 @@ export default class List {
     return false;
   }
 
+  /**
+   * Adds new element to end of list
+   * @param element
+   */
   append(element) {
     this.dataStore[this.listSize++] = element;
   }
 
+  /**
+   * Removes element from list
+   * @param element
+   * @return {boolean}
+   */
   remove(element) {
     const foundAt = this.find(element);
 
@@ -60,42 +78,75 @@ export default class List {
     return false;
   }
 
+  /**
+   * Sets current position to first element of list
+   */
   front() {
     this.pos = 0;
   }
 
+  /**
+   * Sets current position to last element of list
+   */
   end() {
     this.pos = this.listSize - 1;
   }
 
+  /**
+   * Moves current position back one element
+   */
   prev() {
     if (this.pos < this.listSize - 1) {
       --this.pos;
     }
   }
 
+  /**
+   * Moves current position forward one element
+   */
   next() {
     if (this.pos < this.listSize - 1) {
       ++this.pos;
     }
   }
 
+  /**
+   * Returns the current position in list
+   * @return {number|*}
+   */
   currPos() {
     return this.pos;
   }
 
+  /**
+   * Moves the current position to specified position
+   * @param position
+   */
   moveTo(position) {
     this.pos = position;
   }
 
+  /**
+   * Returns element at current position
+   * @return {*}
+   */
   getElement() {
     return this.dataStore[this.pos];
   }
 
+  /**
+   * Returns the number of elements in list
+   * @return {number|*}
+   */
   length() {
     return this.listSize;
   }
 
+  /**
+   * Determining if a Given Value Is in a List
+   * @param element
+   * @return {boolean}
+   */
   contains(element) {
     for (let i = 0; i < this.dataStore.length; ++i) {
       if (this.dataStore[i] === element) {
